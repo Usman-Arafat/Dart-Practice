29b555e376fc1de1dad3ee35cc37f068e9478dc5
Usman <usmanarafat2@gmail.com>
2024-07-01 17:08:46 +0500

    Learn Variables

---
+++ Variables.dart
@@ -0,0 +1,16 @@
+void main(){
+print("A Trip to another school");
+print("------------------------");
+String q="The total number of students present on the trip were";
+int w=4;
+print(q);
+print(w);
+String e="Hashim was not present";
+bool r=true;
+print(e);
+print(r);
+String t="Every one Enjoyed";
+String y="Everyone went by bus";
+print(y);
+print(t);
+}
