371b9772fe52f688c3eccbba3e68f22fe5f9b428
Usman <usmanarafat2@gmail.com>
2024-07-01 22:49:34 +0500

    Map Practice

---
+++ Map.dart
@@ -0,0 +1,24 @@
+void main(){
+
+print("First visit at Library");
+
+print("-----------------------");
+
+print("The No. of students present were ");
+
+int q=2;
+
+print(q);
+
+Map<String, String> w={
+
+"Name 1": "Sajid",
+"Age": "8",
+"Name 2": "Mehdi",
+"Age ": "10"
+
+};
+
+print(w);
+
+}
