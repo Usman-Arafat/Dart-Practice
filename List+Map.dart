e214e020c1646fe89f6dc94c66becb40833e1bf3
Usman <usmanarafat2@gmail.com>
2024-07-02 15:58:59 +0500

    Learn How to Integrate List with Map

---
+++ List+Map.dart
@@ -0,0 +1,31 @@
+void main (){
+print("The students learning JAVA ");
+print("---------------------------");
+List<Map<String , String>> w=[{
+
+"Name":"Aaliyan",
+"Age":"15",
+"Class":"11",
+}
+,{
+"Name":"Sadiq",
+"Age":"14",
+"Class":"19",
+
+}
+,{
+"Name":"Bilal",
+"Age":"13",
+"Class":"7",
+
+}
+];
+print(w);
+
+
+
+
+
+
+
+}
